723697b88b32f7e6ba31fe925c7e7fa08f32b478
Daniel <daniel.davtyan@softconstruct.com>
2025-05-29 13:05:02 +0400

    enabled protected mode

--- boot.asm
+++ boot.asm
@@ -1,89 +1,77 @@
-ORG 0
-BITS 16 
+ORG 0x7c00
+BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
     jmp short start
     nop
 
  times 33 db 0
+ 
 start:
-    jmp 0x7c0:step2
-
-
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    ; mov bx, 0x00
-    int 0x10
-    iret
-
-hanlde_one:
-    mov ah, 0eh
-    mov al, 'V'
-    ; mov bx, 0x00
-
-    int 0x10
-    iret
+    jmp 0:step2
 
 step2:
-    cli ; Clear the interupts
-    mov ax, 0x7c0
+    cli ; Clear Interrupts
+    mov ax, 0x00
     mov ds, ax
     mov es, ax
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-    sti ; Enables interupts
+    sti ; Enables Interrupts
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x116
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    mov word[ss:0x04], hanlde_one
-    mov word[ss:0x06], 0x7c0
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-    mov ax, 0x00
-    div ax
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
-    int 1
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
-    mov ah, 2
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov bx, buffer
-    int 0x13
-    jc error
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
-error:
-    mov si, error_message
-    call print
-    jmp $
 
-print:
-    mov bx, 0
-
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-error_message: db 'Failed to load sector', 0
-
-message: db 'Hello World!', 0
 times 510-($ - $$) db 0
-dw 0xAA55 ; For 512 bytes boot signuture needs to be.
-
-
-buffer: 
+dw 0xAA55
